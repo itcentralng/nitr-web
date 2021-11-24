8684f92ec4eda1a53b8b42f211fa4cb77068f829
Jamilu Salisu <jamilusalis@gmail.com>
2021-11-24 09:52:52 +0100

    added MDB dependecies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// MDB Modules
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+// END of MDB Modules
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaintenanceComponent } from './pages/maintenance/maintenance.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NewsComponent } from './pages/news/news.component';
 import { DepartmentsComponent } from './pages/departments/departments.component';
-import { TopNavbarComponent } from './top-navbar/top-navbar.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { AboutNitrComponent } from './about-nitr/about-nitr.component';
-import { LatestNewsComponent } from './latest-news/latest-news.component';
-import { UpcomingEventsComponent } from './upcoming-events/upcoming-events.component';
-import { FooterCopyrightComponent } from './footer-copyright/footer-copyright.component';
+import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { AboutNitrComponent } from './components/about-nitr/about-nitr.component';
+import { LatestNewsComponent } from './components/latest-news/latest-news.component';
+import { UpcomingEventsComponent } from './components/upcoming-events/upcoming-events.component';
+import { FooterCopyrightComponent } from './components/footer-copyright/footer-copyright.component';
 
 @NgModule({
   declarations: [
@@ -26,13 +44,26 @@ import { FooterCopyrightComponent } from './footer-copyright/footer-copyright.co
     AboutNitrComponent,
     LatestNewsComponent,
     UpcomingEventsComponent,
-    FooterCopyrightComponent
+    FooterCopyrightComponent,
+
+    MdbAccordionModule,
+    MdbCarouselModule,
+    MdbCheckboxModule,
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbFormsModule,
+    MdbModalModule,
+    MdbPopoverModule,
+    MdbRadioModule,
+    MdbRangeModule,
+    MdbRippleModule,
+    MdbScrollspyModule,
+    MdbTabsModule,
+    MdbTooltipModule,
+    MdbValidationModule,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
