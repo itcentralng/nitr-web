8735678dac9abde49a10b67ddf2e431dd34d2642
Jamilu Salisu <jamilusalis@gmail.com>
2021-11-25 15:14:17 +0100

    created a landing page

--- src/app/components/footer-copyright/footer-copyright.component.ts
+++ src/app/components/footer-copyright/footer-copyright.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer-copyright',
   templateUrl: './footer-copyright.component.html',
-  styleUrls: ['./footer-copyright.component.css']
+  styleUrls: ['./footer-copyright.component.css'],
 })
 export class FooterCopyrightComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
