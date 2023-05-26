181a3b37dc10d02f71f1a047af1e1f7e3b58dc8f
Nasir Mustapha <teboy.com@gmail.com>
2023-05-26 11:38:12 +0100

    update production envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API: 'https://admin.nitr.gov.ng/',
-  STORAGE: 'https://nitr.fra1.digitaloceanspaces.com/',
+  API: 'https://admin.nitr.gov.ng/api/',
+  STORAGE: 'https://admin.nitr.gov.ng',
 };
